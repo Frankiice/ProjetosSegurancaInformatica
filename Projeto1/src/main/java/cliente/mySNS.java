7b6ad837834195ac0c36ca188454b880d62c68f2
Francisco <93595934+Frankiice@users.noreply.github.com>
2024-03-19 21:36:45 +0000

    Adicionar TCP, sockets no server, e ifs/switch case para opcoes

--- Projeto1/src/main/java/cliente/mySNS.java
+++ Projeto1/src/main/java/cliente/mySNS.java
@@ -1,5 +1,70 @@
 package cliente;
 
-public class mySNS {
+import javax.crypto.*;
+//import javax.net.SocketFactory;
+//import javax.net.ssl.SSLSocketFactory;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+//import java.util.Scanner;
+import java.util.Map;
 
+public class mySNS {
+	public static void main(String[] args) throws Exception,IOException, ClassNotFoundException {
+		if (args.length < 5) {
+            System.out.println("Argumentos invalidos (ex: -a <serverAddress> -u <username do utente> -g {<filenames>}+).");
+            System.exit(0);        
+        }
+		if (!args[0].equals("-a")) {
+            System.out.println("Eh preciso fornecer a opcao -a (ex: -a <serverAddress> )");
+            System.exit(0);
+        }
+		String[] parts = args[1].split(":");
+        if (parts.length != 2) {
+            System.out.println("Eh preciso fornecer um endereco IP e o porto do servidor (ex: 127.0.0.1:23456).");
+            System.exit(0);
+        }
+        //System.out.println("servidor:" + args[1]);
+        //criar socket para ligar ao server
+        Socket echoSocket = new Socket(parts[0], Integer.parseInt(parts[1])); //127.0.0.1
+        //criar stream object
+        ObjectInputStream in = new ObjectInputStream(echoSocket.getInputStream()); 
+        ObjectOutputStream out = new ObjectOutputStream(echoSocket.getOutputStream());
+        
+        String opcaoUsername = args[2];
+        String opcao = "";
+        out.writeObject(opcaoUsername);
+        if(opcaoUsername.equals("-m")) {
+        	opcao = args[6];
+        }
+        if(opcaoUsername.equals("-u")) {
+        	opcao = args[4];
+        }
+        switch(opcao) {
+	        case "-sc":
+	        	//
+	        case "-sa":
+	        	//
+	        case "-se":
+	        	//
+	        case "-g":
+	        	//
+	        case "":
+        }
+        
+	}
 }
--- Projeto1/src/main/java/servidor/mySNSServer.java
+++ Projeto1/src/main/java/servidor/mySNSServer.java
@@ -1,5 +1,111 @@
 package servidor;
 
+import javax.crypto.*;
+//import javax.net.SocketFactory;
+//import javax.net.ssl.SSLSocketFactory;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+//import java.util.Scanner;
+import java.util.Map;
 public class mySNSServer {
-
+	
+	public static void main(String[] args) {
+		System.out.println("servidor: main");
+		if (args.length==1 && isConvertibleToInt(args[0])) {
+			mySNSServer server = new mySNSServer();
+			server.startServer(Integer.parseInt(args[0]));
+		}
+		else {
+			System.out.println("AVISO: Estrutura do comando incorreta!");
+			System.out.println("ESTRUTURA CORRETA: java mySNSServer <portNumber>");
+			System.out.println("EXEMPLO: java mySNSServer 57160");
+			System.exit(0);
+		}
+		
+	}
+	
+	public void startServer(int port) {
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+			System.exit(-1);
+		}
+		
+		while (true) {
+			try {
+				Socket inSoc = serverSocket.accept();
+				ServerThread newServerThread = new ServerThread(inSoc);
+				newServerThread.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	class ServerThread extends Thread {
+		private Socket socket = null;
+		ServerThread(Socket inSoc) {
+			socket = inSoc;
+			System.out.println("thread do server para cada cliente");
+		}
+		public void run() {
+			try {
+				ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+				String[] nomesFicheiros = (String[]) inStream.readObject();
+				if (nomesFicheiros.length!=0) {
+					if (nomesFicheiros[0].equals("REQ")) {
+						//sendFiles(socket,outStream,inStream,removerPrimeiroElemento(nomesFicheiros));
+					} 
+					else {
+						//receiveFiles(socket,outStream,inStream,nomesFicheiros);
+					}
+				}
+				else {
+					String[] arrayVazio = {};
+					outStream.writeObject(arrayVazio);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException c) {
+				c.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	
+	
+	public static boolean isConvertibleToInt(String str) {
+	    try {
+	        Integer.parseInt(str);
+	        return true;
+	    } catch (NumberFormatException e) {
+	        return false;
+	    }
+	}
+	
+	
+	
 }

730413b971544440173318d709094129d0f2218f
Francisco <93595934+Frankiice@users.noreply.github.com>
2024-04-01 22:13:57 +0100

    Assinatura Gabriel, inicio de -g option verificacao assinatura

--- Projeto1/src/main/java/cliente/mySNS.java
+++ Projeto1/src/main/java/cliente/mySNS.java
@@ -4,14 +4,11 @@ import javax.crypto.*;
 //import javax.net.SocketFactory;
 //import javax.net.ssl.SSLSocketFactory;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
@@ -24,16 +21,16 @@ import java.util.List;
 import java.util.Map;
 
 public class mySNS {
-	public static void main(String[] args) throws Exception,IOException, ClassNotFoundException {
-		if (args.length < 5) {
+    public static void main(String[] args) throws Exception,IOException, ClassNotFoundException {
+        if (args.length < 5) {
             System.out.println("Argumentos invalidos (ex: -a <serverAddress> -u <username do utente> -g {<filenames>}+).");
-            System.exit(0);        
+            System.exit(0);
         }
-		if (!args[0].equals("-a")) {
+        if (!args[0].equals("-a")) {
             System.out.println("Eh preciso fornecer a opcao -a (ex: -a <serverAddress> )");
             System.exit(0);
         }
-		String[] parts = args[1].split(":");
+        String[] parts = args[1].split(":");
         if (parts.length != 2) {
             System.out.println("Eh preciso fornecer um endereco IP e o porto do servidor (ex: 127.0.0.1:23456).");
             System.exit(0);
@@ -42,29 +39,309 @@ public class mySNS {
         //criar socket para ligar ao server
         Socket echoSocket = new Socket(parts[0], Integer.parseInt(parts[1])); //127.0.0.1
         //criar stream object
-        ObjectInputStream in = new ObjectInputStream(echoSocket.getInputStream()); 
+        ObjectInputStream in = new ObjectInputStream(echoSocket.getInputStream());
         ObjectOutputStream out = new ObjectOutputStream(echoSocket.getOutputStream());
-        
+
         String opcaoUsername = args[2];
         String opcao = "";
         out.writeObject(opcaoUsername);
-        if(opcaoUsername.equals("-m")) {
-        	opcao = args[6];
+        switch (opcaoUsername){
+            case "-u":
+                out.writeObject(args[3]);
+                opcao = "-g";
+                break;
+            case "-m":
+                opcao = args[6];
+                out.writeObject(args[3]);
+                out.writeObject(args[5]);
+                break;
         }
-        if(opcaoUsername.equals("-u")) {
-        	opcao = args[4];
-        }
+        //Envia ao servidor o comando a executar
+        out.writeObject(opcao);
         switch(opcao) {
-	        case "-sc":
-	        	//
-	        case "-sa":
-	        	//
-	        case "-se":
-	        	//
-	        case "-g":
-	        	//
-	        case "":
+            case "-sc":
+                DoScOption(args, in, out);
+                break;
+            case "-sa":
+                DoSaOption(args, in, out);
+            case "-se":
+                //
+            case "-g":
+                System.out.println("inside g option");
+                //doGoption(args, in, out);
+            case "":
         }
-        
-	}
+
+
+    }
+
+
+
+
+    public static void DoScOption(String [] args, ObjectInputStream in, ObjectOutputStream out) throws IOException, ClassNotFoundException {
+        //Envio a quantidade de ficheiros para o loop do lado do servidor saber quantas vezes
+        //ler o InputStream
+        out.writeObject(args.length-6);
+        for (int i = args.length-1; i>=6; i--){
+
+            if(PodeCifrar(args[i], in, out)){
+                //FaÃ§o a cifra hibrida
+                CifraHibrida(args[i], args[3]);
+                //Envio para o servidor
+                File ficheiroCifrado = new File(args[i]+".cifrado");
+                out.writeObject(ficheiroCifrado);
+                File chaveSecretaCifrada = new File(args[i]+".chave_secreta."+args[3]);
+                out.writeObject(chaveSecretaCifrada);
+            }
+
+            else
+                System.out.println("Erro ao cifrar ficheiro " + args[i]);
+        }
+
+    }
+
+    private static void CifraHibrida(String arg, String utente) {
+        try {
+            //Cria o ficheiro cifrado com o nome <arg>.cifrado e retorna a chave usada
+            SecretKey secretKey = DoCifraSimetrica(arg);
+            //Cifra a chave simetrica e escreve no ficheiro <arg>.chave_secreta.<utente>
+            CifraChaveSimetrica(secretKey, arg, utente);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void CifraChaveSimetrica(SecretKey secretKey, String nomeFicheiro, String utente) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
+        //Gera par de chaves
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        //Cifra a chave simetrica
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
+        byte[] chaveCifrada = cipher.doFinal(secretKey.getEncoded());
+
+        //Escreve para um ficheiro
+        FileOutputStream fos = new FileOutputStream(nomeFicheiro + ".chave_secreta." + utente);
+        fos.write(chaveCifrada);
+        fos.close();
+    }
+
+    private static byte[] readFile(File file) {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] data = new byte[(int) file.length()];
+            fileInputStream.read(data);
+            return data;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+
+    private static SecretKey DoCifraSimetrica(String arg) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IOException {
+
+        //Gera a chave simetrica
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        SecretKey secretKey = keyGen.generateKey();
+
+        //Cifra o ficheiro
+        File inputFile = new File(arg);
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] inputBytes = readFile(inputFile);
+
+        //Escreve para o ficheiro
+        FileOutputStream fos = new FileOutputStream(arg + ".cifrado");
+        fos.write(inputBytes);
+        //colocar o write byte
+        fos.close();
+        return secretKey;
+    }
+
+    public static boolean PodeCifrar(String nomeFicheiro, ObjectInputStream in, ObjectOutputStream out) throws IOException, ClassNotFoundException {
+        Boolean podeCifrar = false;
+        File arquivo = new File(nomeFicheiro);
+        podeCifrar = arquivo.exists();
+        out.writeObject(nomeFicheiro);
+        podeCifrar = !(Boolean) in.readObject();
+        return podeCifrar;
+    }
+
+    // Gabriel
+
+    private static void doGoption(String[] args, ObjectInputStream in, ObjectOutputStream out) throws IOException {
+        try {
+            // Envia a quantidade de ficheiros
+            out.writeInt(args.length-5);
+
+            // Senha padrão da keystore
+            String defaultKeystorePassword = "123456";
+            String aliasUtente = args[3];
+
+            String aliasMedico = "silva";
+
+            // Obter a chave publica da keystore do medico
+            PublicKey publicKey = getPublicKey(aliasMedico, defaultKeystorePassword);
+
+            // envia quantidade de ficheiros
+            //out.writeInt(args.length-7);
+            // Assinar cada arquivo
+            for (int i = 5; i < args.length; i++) {
+                System.out.println("inside for loop");
+                //verificaAssinatura(args[i], in, out);
+                
+                //assinaFicheiro(args[i], aliasMedico, aliasUtente, privateKey, defaultKeystorePassword, in, out);
+            }
+        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | FileNotFoundException | UnrecoverableKeyException e) {
+            System.out.println("Ocorreu um erro, verifique se existe keystore para o medico e tente novamente");
+            System.out.println();
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean verificaAssinatura(String filename, ObjectInputStream in, ObjectOutputStream out) throws IOException {
+        // Ler o arquivo original
+        File file = new File(filename);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] fileBytes = new byte[(int) file.length()];
+        fileInputStream.read(fileBytes);
+
+        // Ler a assinatura do arquivo
+
+        //String signatureFilename = (String) in.readUTF();
+        //byte[] signatureBytes = (byte[]) in.readObject();
+
+        //String signedFilename = (String) in.readUTF();
+        //byte[] signedBytes = (byte[]) in.readObject();
+
+        //FileInputStream signatureInputStream = new FileInputStream(signatureFilename);
+        //byte[] signatureBytes = new byte[(int) file.length()];
+        //signatureInputStream.read(signatureBytes);
+
+        // Criar objeto Signature para verificar a assinatura
+        //Signature signature = Signature.getInstance("MD5withRSA");
+        //signature.initVerify(publicKey);
+
+        // Atualizar a assinatura com os dados do arquivo original
+        //signature.update(fileBytes);
+
+        // Verificar a assinatura
+        //return signature.verify(signatureBytes);
+        return false;
+    }
+    public static void DoSaOption(String [] args, ObjectInputStream in, ObjectOutputStream out) throws IOException, ClassNotFoundException {
+        try {
+            String defaultKeystorePassword = "123456"; // Senha padrão da keystore
+            String aliasMedico = args[3];
+            String aliasUtente = args[5];
+
+            // Obter a chave privada da keystore
+            PrivateKey privateKey = getPrivateKey(aliasMedico, defaultKeystorePassword);
+
+            // envia quantidade de ficheiros
+            out.writeInt(args.length-7);
+            // Assinar cada arquivo
+            for (int i = 7; i < args.length; i++) {
+                assinaFicheiro(args[i], aliasMedico, aliasUtente, privateKey, defaultKeystorePassword, in, out);
+            }
+        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | FileNotFoundException | UnrecoverableKeyException e) {
+            System.out.println("Ocorreu um erro, verifique se existe keystore para o medico e tente novamente");
+            System.out.println();
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void assinaFicheiro(String filename, String aliasMedico, String aliasUtente, PrivateKey privateKey, String keystorePassword, ObjectInputStream in, ObjectOutputStream out) throws IOException, ClassNotFoundException {
+
+        try {
+            // Criar objeto Signature para assinar o arquivo
+            Signature signature = Signature.getInstance("MD5withRSA");
+            signature.initSign(privateKey);
+
+            // Ler o arquivo para assinar
+            File file = new File(filename);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            byte[] fileBytes = new byte[(int) file.length()];
+            fileInputStream.read(fileBytes);
+
+            // Atualizar a assinatura com os dados do arquivo
+            signature.update(fileBytes);
+
+            // Gerar a assinatura
+            byte[] signatureBytes = signature.sign();
+            Boolean servidorRecebeu;
+            Boolean ficheiroDuplicado;
+            // Enviar o arquivo assinado para o servidor
+            out.writeUTF(filename + ".assinado");
+            out.flush();
+
+            // Recebe do servidor um boolean que e true caso o ficheiro ja exista no servidor.
+            ficheiroDuplicado = (Boolean) in.readBoolean();
+            if(ficheiroDuplicado)
+            {
+                System.out.println("O ficheiro " + filename + " ja existe no diretorio de " + aliasUtente);
+                return;
+            }
+            else{
+                System.out.println("Ficheiro " + filename + " foi enviado para o servidor no diretorio de " + aliasUtente);
+            }
+
+            out.writeObject(fileBytes);
+            out.flush();
+            servidorRecebeu = (Boolean) in.readBoolean();
+
+            // Enviar a assinatura para o servidor
+            out.writeUTF(filename + ".assinatura." + aliasMedico);
+            out.flush();
+            out.writeObject(signatureBytes);
+            out.flush();
+            servidorRecebeu = (Boolean) in.readBoolean();
+
+        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void salvaFicheiro(String filename, byte[] fileBytes) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(filename)) {
+            fileOutputStream.write(fileBytes);
+        }
+    }
+    //verificaAssinatura(args[i], aliasMedico, aliasUtente, publicKey, defaultKeystorePassword, in, out);
+
+
+    private static PublicKey getPublicKey(String alias, String password) throws Exception {
+        // Carregar a keystore do servidor...
+        FileInputStream keystoreInputStream = new FileInputStream("keystores/" + alias + ".keystore");
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        keyStore.load(keystoreInputStream, password.toCharArray());
+
+        // Obter o certificado do alias especificado...
+        Certificate certificate = keyStore.getCertificate(alias);
+
+        // Retornar a chave pública do certificado...
+        return certificate.getPublicKey();
+    }
+
+    private static PrivateKey getPrivateKey(String alias, String password) throws Exception {
+        // Carregar a keystore do usuário
+        String keystoreName = alias + ".keystore"; // Supondo que o nome da keystore é passado como argumento
+        String keystorePath = "keystores/" + keystoreName;
+        FileInputStream keystoreInputStream = new FileInputStream(keystorePath);
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        keyStore.load(keystoreInputStream, password.toCharArray()); // Usando a senha padrão da keystore
+
+        // Obter a chave privada da keystore
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, password.toCharArray()); // Usando a senha padrão da keystore
+        return privateKey;
+    }
+
 }

c3ebc63bc760cf3b3ff8ef7e952b2903e4b2daf6
Felipe Cardoso <felcarbib@gmail.com>
2024-04-03 21:11:34 +0100

    Opt -sc

--- Projeto1/src/main/java/cliente/mySNS.java
+++ Projeto1/src/main/java/cliente/mySNS.java
@@ -1,16 +1,9 @@
-package cliente;
-
 import javax.crypto.*;
 //import javax.net.SocketFactory;
 //import javax.net.ssl.SSLSocketFactory;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+
+import java.io.*;
 import java.net.Socket;
 import java.security.*;
 import java.security.cert.Certificate;
@@ -24,16 +17,16 @@ import java.util.List;
 import java.util.Map;
 
 public class mySNS {
-	public static void main(String[] args) throws Exception,IOException, ClassNotFoundException {
-		if (args.length < 5) {
+    public static void main(String[] args) throws Exception,IOException, ClassNotFoundException {
+        if (args.length < 5) {
             System.out.println("Argumentos invalidos (ex: -a <serverAddress> -u <username do utente> -g {<filenames>}+).");
-            System.exit(0);        
+            System.exit(0);
         }
-		if (!args[0].equals("-a")) {
+        if (!args[0].equals("-a")) {
             System.out.println("Eh preciso fornecer a opcao -a (ex: -a <serverAddress> )");
             System.exit(0);
         }
-		String[] parts = args[1].split(":");
+        String[] parts = args[1].split(":");
         if (parts.length != 2) {
             System.out.println("Eh preciso fornecer um endereco IP e o porto do servidor (ex: 127.0.0.1:23456).");
             System.exit(0);
@@ -42,29 +35,155 @@ public class mySNS {
         //criar socket para ligar ao server
         Socket echoSocket = new Socket(parts[0], Integer.parseInt(parts[1])); //127.0.0.1
         //criar stream object
-        ObjectInputStream in = new ObjectInputStream(echoSocket.getInputStream()); 
+        ObjectInputStream in = new ObjectInputStream(echoSocket.getInputStream());
         ObjectOutputStream out = new ObjectOutputStream(echoSocket.getOutputStream());
-        
+
         String opcaoUsername = args[2];
-        String opcao = "";
+        String opcao = args[6];
         out.writeObject(opcaoUsername);
-        if(opcaoUsername.equals("-m")) {
-        	opcao = args[6];
+        switch (opcaoUsername){
+            case "-u":
+                out.writeObject(args[3]);
+                opcao = "-g";
+                break;
+            case "-m":
+                out.writeObject(args[3]);
+                out.writeObject(args[5]);
+                break;
         }
-        if(opcaoUsername.equals("-u")) {
-        	opcao = args[4];
-        }
+        //Envia ao servidor o comando a executar
+        out.writeObject(args[6]);
         switch(opcao) {
-	        case "-sc":
-	        	//
-	        case "-sa":
-	        	//
-	        case "-se":
-	        	//
-	        case "-g":
-	        	//
-	        case "":
+            case "-sc":
+                DoScOption(args, in, out);
+                break;
+            case "-sa":
+                //
+            case "-se":
+                //
+            case "-g":
+                //
+            case "":
         }
-        
-	}
+
+
+    }
+    public static void DoScOption(String [] args, ObjectInputStream in, ObjectOutputStream out) throws IOException, ClassNotFoundException {
+        //Envio a quantidade de ficheiros para o loop do lado do servidor saber quantas vezes
+        //ler o InputStream
+        out.writeObject((args.length-7)*2);
+        for (int i = args.length-1; i>=7; i--){
+
+            if(PodeCifrar(args[i], in, out)){
+                //Faço a cifra hibrida
+                CifraHibrida(args[i], args[5]);
+                //Envio para o servidor
+                File ficheiroCifrado = new File(args[i]+".cifrado");
+                out.writeObject(ficheiroCifrado);
+                File chaveSecretaCifrada = new File(args[i]+".chave_secreta."+args[5]);
+                out.writeObject(chaveSecretaCifrada);
+            }
+
+            else
+                System.out.println("Erro ao cifrar ficheiro " + args[i]);
+        }
+
+    }
+
+    private static void CifraHibrida(String arg, String utente) {
+        try {
+            //Cria o ficheiro cifrado com o nome <arg>.cifrado e retorna a chave usada
+            SecretKey secretKey = DoCifraSimetrica(arg);
+            //Cifra a chave simetrica e escreve no ficheiro <arg>.chave_secreta.<utente>
+            CifraChaveSimetrica(secretKey, arg, utente);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void CifraChaveSimetrica(SecretKey secretKey, String nomeFicheiro, String utente) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException, CertificateException, KeyStoreException, UnrecoverableKeyException {
+        //Carregar keystore
+        String keystorePass = "123456";
+        String keystorePath = "keystores/" + utente + ".keystore";
+        FileInputStream keystoreInputStream = new FileInputStream(keystorePath);
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        keyStore.load(keystoreInputStream, keystorePass.toCharArray());
+
+        //Gera par de chaves
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey(utente, keystorePass.toCharArray());
+        PublicKey publicKey = keyStore.getCertificate(utente).getPublicKey();
+
+        //Cifra a chave simetrica
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] chaveCifrada = cipher.doFinal(secretKey.getEncoded());
+
+        //Escreve para um ficheiro
+        FileOutputStream fos = new FileOutputStream(nomeFicheiro + ".chave_secreta." + utente);
+        FileInputStream inputFile;
+
+        inputFile = new FileInputStream(nomeFicheiro + ".chave_secreta." + utente);
+        CipherOutputStream cos = new CipherOutputStream(fos, cipher);
+
+        byte[]b = new byte[256];
+        int i = inputFile.read(b);
+        while(i !=-1){
+            cos.write(chaveCifrada,0,i);
+            i=inputFile.read();
+        }
+        cos.close();
+        inputFile.close();
+        fos.close();
+    }
+
+    private static byte[] readFile(File file) {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] data = new byte[(int) file.length()];
+            fileInputStream.read(data);
+            return data;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+
+    private static SecretKey DoCifraSimetrica(String arg) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IOException {
+        //Gera a chave simetrica
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        SecretKey secretKey = keyGen.generateKey();
+
+        FileInputStream inputFile;
+        CipherOutputStream cos;
+
+        //Cifra o ficheiro
+        inputFile = new FileInputStream(arg);
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        FileOutputStream fos = new FileOutputStream(arg + ".cifrado");
+        cos = new CipherOutputStream(fos, cipher);
+
+        byte[]b = new byte[256];
+        int i = inputFile.read(b);
+        while(i !=-1){
+            cos.write(b,0,i);
+            i=inputFile.read();
+        }
+
+        cos.close();
+        inputFile.close();
+        fos.close();
+        return secretKey;
+    }
+
+    public static boolean PodeCifrar(String nomeFicheiro, ObjectInputStream in, ObjectOutputStream out) throws IOException, ClassNotFoundException {
+        Boolean podeCifrar = false;
+        File arquivo = new File(nomeFicheiro);
+        podeCifrar = arquivo.exists();
+        out.writeObject(nomeFicheiro);
+        podeCifrar = !(Boolean) in.readObject();
+        return podeCifrar;
+    }
 }
--- Projeto1/src/main/java/servidor/mySNSServer.java
+++ Projeto1/src/main/java/servidor/mySNSServer.java
@@ -1,30 +1,17 @@
-package servidor;
-
-import javax.crypto.*;
 //import javax.net.SocketFactory;
 //import javax.net.ssl.SSLSocketFactory;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.*;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 //import java.util.Scanner;
-import java.util.Map;
-public class mySNSServer {
-	
+
+
+class mySNSServer {
+
 	public static void main(String[] args) {
 		System.out.println("servidor: main");
 		if (args.length==1 && isConvertibleToInt(args[0])) {
@@ -37,18 +24,19 @@ public class mySNSServer {
 			System.out.println("EXEMPLO: java mySNSServer 57160");
 			System.exit(0);
 		}
-		
+
 	}
-	
+
 	public void startServer(int port) {
 		ServerSocket serverSocket = null;
+
 		try {
 			serverSocket = new ServerSocket(port);
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 			System.exit(-1);
 		}
-		
+
 		while (true) {
 			try {
 				Socket inSoc = serverSocket.accept();
@@ -57,33 +45,62 @@ public class mySNSServer {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+
 		}
-		
+
 	}
-	
+
 	class ServerThread extends Thread {
+
 		private Socket socket = null;
+
 		ServerThread(Socket inSoc) {
 			socket = inSoc;
 			System.out.println("thread do server para cada cliente");
 		}
+
 		public void run() {
 			try {
 				ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
 				ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
-				String[] nomesFicheiros = (String[]) inStream.readObject();
-				if (nomesFicheiros.length!=0) {
-					if (nomesFicheiros[0].equals("REQ")) {
-						//sendFiles(socket,outStream,inStream,removerPrimeiroElemento(nomesFicheiros));
-					} 
+				//String[] nomesFicheiros = (String[]) inStream.readObject();
+				String opcaoCliente = (String) inStream.readObject();
+				String nomeUtente = "";
+				if(!opcaoCliente.isBlank()) {
+					if(opcaoCliente.equals("-m")) {
+						String nomeMedico = (String) inStream.readObject();
+						nomeUtente = (String) inStream.readObject();
+						criaDiretorio(nomeMedico);
+						criaDiretorio(nomeUtente);
+					}
+					else if(opcaoCliente.equals("-u")) {
+						nomeUtente = (String) inStream.readObject();
+						criaDiretorio(nomeUtente);
+					}
 					else {
-						//receiveFiles(socket,outStream,inStream,nomesFicheiros);
+						outStream.writeBoolean(false);
+						System.out.println("opção incorreta pelo cliente");
 					}
 				}
-				else {
-					String[] arrayVazio = {};
-					outStream.writeObject(arrayVazio);
+				//Recebe o comando a se fazer
+				String opcao = (String) inStream.readObject();
+				switch(opcao) {
+					case "-sc":
+						int qtdFicheiros =(int) inStream.readObject();
+						for(int i = 0; i < qtdFicheiros; i+=2){
+							VerificaFicheiro(qtdFicheiros,inStream, outStream, nomeUtente);
+							RecebeFicheiros(qtdFicheiros,nomeUtente,outStream,inStream);
+						}
+						break;
+					case "-sa":
+						//
+					case "-se":
+						//
+					case "-g":
+						//
+					case "":
 				}
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -91,21 +108,64 @@ public class mySNSServer {
 				c.printStackTrace();
 			}
 		}
-		
-		
+
+		private void RecebeFicheiros(int qtdFicheiros,String utente, ObjectOutputStream out, ObjectInputStream in) throws IOException, ClassNotFoundException {
+
+			Path diretorioUtente = Paths.get(utente);
+			if (!Files.exists(diretorioUtente)) {
+				Files.createDirectories(diretorioUtente);
+			}
+			// Recebe o arquivo cifrado
+			File ficheiroCifrado = (File) in.readObject();
+			// Define o caminho do arquivo cifrado no diretório do usuário
+			Path caminhoFicheiroCifrado = diretorioUtente.resolve(ficheiroCifrado.getName());
+			// Copia o arquivo cifrado para o diretório do usuário
+			Files.copy(ficheiroCifrado.toPath(), caminhoFicheiroCifrado);
+
+			// Recebe o arquivo da chave cifrada
+			File chaveCifrada = (File) in.readObject();
+			// Define o caminho do arquivo da chave cifrada no diretório do usuário
+			Path caminhoChaveCifrada = diretorioUtente.resolve(chaveCifrada.getName());
+			// Copia o arquivo da chave cifrada para o diretório do usuário
+			Files.copy(chaveCifrada.toPath(), caminhoChaveCifrada);
+		}
+
+
+		// Cria o diretório do usuário se não existir
+
+		public static void criaDiretorio(String nomeUtente){
+			File diretorio = new File(nomeUtente);
+
+			if (!diretorio.exists()) {
+				// Se o diretório não existir, crie-o
+				boolean criadoComSucesso = diretorio.mkdir();
+				if (criadoComSucesso) {
+					System.out.println("Diretório criado com sucesso.");
+				} else {
+					System.out.println("Falha ao criar o diretório.");
+				}
+			} else {
+				System.out.println("O diretório já existe.");
+			}
+		}
+
+		public static void VerificaFicheiro(int qtd, ObjectInputStream inStream, ObjectOutputStream outStream, String nomeUtente){
+			try {
+				String nomeFicheiro = (String) inStream.readObject();
+				File arquivo = new File(nomeUtente +"/"+nomeFicheiro);
+				outStream.writeObject(arquivo.exists());
+			} catch (IOException | ClassNotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
 	}
-	
-	
-	
 	public static boolean isConvertibleToInt(String str) {
-	    try {
-	        Integer.parseInt(str);
-	        return true;
-	    } catch (NumberFormatException e) {
-	        return false;
-	    }
+		try {
+			Integer.parseInt(str);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
-	
-	
-	
 }
